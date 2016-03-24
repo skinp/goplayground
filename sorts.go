a4e51e9d1324f791dbc2e1b8de0420e21af6ebcd
Patrick Pelletier <pp.pelletier@gmail.com>
2016-03-23 18:19:21 -0700

    add examples of how to sort a map by key and by value

--- sorts.go
+++ sorts.go
@@ -1,5 +1,7 @@
 package myplayground
 
+import "sort"
+
 // Person is used to show how we can sort collections of complex objects. We
 // need to implement the 3 methods Len, Swap and Less on a collection type.
 // Whenever we have an array/slice []Person, we can sort it as follow:
@@ -9,9 +11,54 @@ type Person struct {
 	Age  int
 }
 
-//PersonSorter sorts Persons by age
+// Less returns whether the Person object is smaller than the other based on
+// their age.
+func (p Person) Less(another Person) bool {
+	return p.Age < another.Age
+}
+
+// PersonSorter sorts Persons by age.
 type PersonSorter []Person
 
 func (s PersonSorter) Len() int           { return len(s) }
 func (s PersonSorter) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
-func (s PersonSorter) Less(i, j int) bool { return s[i].Age < s[j].Age }
+func (s PersonSorter) Less(i, j int) bool { return s[i].Less(s[j]) }
+
+// PersonPair is an intermediate data structure that helps us sort a map of
+// Persons.
+type PersonPair struct {
+	Name   string
+	Person Person
+}
+
+// PersonPairSorter sorts a map of Persons by age.
+type PersonPairSorter []PersonPair
+
+func (s PersonPairSorter) Len() int           { return len(s) }
+func (s PersonPairSorter) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s PersonPairSorter) Less(i, j int) bool { return s[i].Person.Less(s[j].Person) }
+
+// SortPersonMapByKey sorts a map of name->Person by key. We just return the
+// list of keys because we assume the caller can just iterate over the keys
+// and use map index to do whatever it wants to do with the order of keys.
+func SortPersonMapByKey(pm map[string]Person) []string {
+	keys := []string{}
+	for k, _ := range pm {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+// SortPersonMapByValue sorts a map of name->Person by value. Because go doesn't
+// allow for sort by value built in like Python, we need to use an intermediate
+// list of pair data structure and sort the list. This list is what we return
+// because map a never sorted in Go.
+func SortPersonMapByValue(pm map[string]Person) []PersonPair {
+	pp := []PersonPair{}
+	for k, v := range pm {
+		pp = append(pp, PersonPair{k, v})
+	}
+	sort.Sort(PersonPairSorter(pp))
+	return pp
+}
--- sorts_test.go
+++ sorts_test.go
@@ -7,17 +7,18 @@ import (
 )
 
 func TestPersonSorter(t *testing.T) {
+	t.Parallel()
 	persons := []Person{
-		{Name: "leonardo", Age: 30},
-		{Name: "donatello", Age: 10},
-		{Name: "michelangelo", Age: 100},
-		{Name: "raphael", Age: 50},
+		{"leonardo", 30},
+		{"donatello", 10},
+		{"michelangelo", 100},
+		{"raphael", 50},
 	}
 	expected := []Person{
-		{Name: "donatello", Age: 10},
-		{Name: "leonardo", Age: 30},
-		{Name: "raphael", Age: 50},
-		{Name: "michelangelo", Age: 100},
+		{"donatello", 10},
+		{"leonardo", 30},
+		{"raphael", 50},
+		{"michelangelo", 100},
 	}
 	if reflect.DeepEqual(persons, expected) {
 		t.Errorf("people should not be in their right order")
@@ -25,5 +26,63 @@ func TestPersonSorter(t *testing.T) {
 	sort.Sort(PersonSorter(persons))
 	if !reflect.DeepEqual(persons, expected) {
 		t.Errorf("people are not in their right order")
+	}
+}
+
+func TestPersonPairSorter(t *testing.T) {
+	t.Parallel()
+	persons := []PersonPair{
+		{"l", Person{"leonardo", 30}},
+		{"d", Person{"donatello", 10}},
+		{"m", Person{"michelangelo", 100}},
+		{"r", Person{"raphael", 50}},
+	}
+	expected := []PersonPair{
+		{"d", Person{"donatello", 10}},
+		{"l", Person{"leonardo", 30}},
+		{"r", Person{"raphael", 50}},
+		{"m", Person{"michelangelo", 100}},
+	}
+	if reflect.DeepEqual(persons, expected) {
+		t.Errorf("people should not be in their right order")
+	}
+	sort.Sort(PersonPairSorter(persons))
+	if !reflect.DeepEqual(persons, expected) {
+		t.Errorf("people are not in their right order")
+	}
+}
+
+func TestSortMapByKey(t *testing.T) {
+	t.Parallel()
+	persons := map[string]Person{
+		"l": Person{"leonardo", 30},
+		"d": Person{"donatello", 10},
+		"m": Person{"michelangelo", 100},
+		"r": Person{"raphael", 50},
+	}
+	expected := []string{"d", "l", "m", "r"}
+	got := SortPersonMapByKey(persons)
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("keys are not in their right order")
+	}
+}
+
+func TestSortMapByValue(t *testing.T) {
+	t.Parallel()
+	persons := map[string]Person{
+		"l": Person{"leonardo", 30},
+		"d": Person{"donatello", 10},
+		"m": Person{"michelangelo", 100},
+		"r": Person{"raphael", 50},
+	}
+	expected := []PersonPair{
+		{"d", Person{"donatello", 10}},
+		{"l", Person{"leonardo", 30}},
+		{"r", Person{"raphael", 50}},
+		{"m", Person{"michelangelo", 100}},
+	}
+	got := SortPersonMapByValue(persons)
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("person pairs are not in their right order")
 	}
 }
